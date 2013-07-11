ebd047aa3328b290176b7c383d21081cf262f14d
Marc Milgrom <mdmilgrom@gmail.com>
2013-07-11 03:27:11 +0000

    Fixed web.js fs

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var text = new String
-  text = fs.readFileSync("index.html").buftoString
+  text = fs.readFileSync("./index.html").buftoString
   response.send(text);
 });
 
