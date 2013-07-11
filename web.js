7e9d98087cd9520f93afc8a40eee5be3efc782e2
Marc Milgrom <mdmilgrom@gmail.com>
2013-07-11 03:40:02 +0000

    Fixed web.js fs

--- web.js
+++ web.js
@@ -4,9 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var text = new String
-  text = fs.readFileSync("./index.html").buftoString
-  response.send(text);
+  response.send(fs.readFileSync("./index.html").buftoString);
 });
 
 var port = process.env.PORT || 5000;
